716d7806677b8696a435597ed46494ab293e3518
KULDEEP GANGWAR <120949786+Kuldeep8057@users.noreply.github.com>
2023-10-30 06:33:51 +0530

    Delete 15th_week_assignment directory

--- 15th_week_assignment/JavaScript4.js
+++
@@ -1,7 +0,0 @@
-function hasProperty(obj, propertyName) {
-    return obj.hasOwnProperty(propertyName);
-}
-
-const example = { name: "Kuldeep", age: 25 };
-console.log(hasProperty(example, "age")); //
-console.log(hasProperty(example, "email"));
