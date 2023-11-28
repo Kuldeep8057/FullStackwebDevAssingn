8cbb05416ad450e238336321864e1df0eb87fafc
KULDEEP GANGWAR <120949786+Kuldeep8057@users.noreply.github.com>
2023-11-28 22:19:46 +0530

    Delete 15th_week_assignment directory

--- 15th_week_assignment/JavaScript5.js
+++
@@ -1,5 +0,0 @@
-const radius = 6;
-const circleArea =  Math.PI * Math.pow(radius,2);
-
-console.log(circleArea)
-    
--- 15th_week_assignment/JavaScript6.js
+++
@@ -1,8 +0,0 @@
-function reverseString (input){
-    return input.split('').reverse().join('')
-}
-
-const originalString = "Hello World";
-
-const reversedString = reverseString(originalString);
-console.log(reversedString)
