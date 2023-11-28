c7b729e93be4e15f7ea01a27f9a074956318f304
KULDEEP GANGWAR <120949786+Kuldeep8057@users.noreply.github.com>
2023-11-28 22:22:17 +0530

    Add files via upload

---
+++ 15th_week_assignment_fundamental/JavaScript2.js
@@ -0,0 +1,26 @@
+const student = {
+    name: "Kuldeep",
+    age: 25,
+    grade: "A"
+}
+
+// student["grade"] = "A+"
+student.grade="A+"
+
+console.log(student)
+
+    // with function    
+
+// const student = {
+//     name: "Kuldeep",
+//     age: 25,
+//     grade: "A"
+//   };
+
+//   // Method to update the student's grade
+//   function updateGrade(newGrade) {
+//     student.grade = newGrade;
+//   }
+
+//   updateGrade("A+");
+//   console.log(student);
---
+++ 15th_week_assignment_fundamental/JavaScript3.js
@@ -0,0 +1,15 @@
+function inputProperty (obj){
+    return Object.keys(obj).length
+}
+
+const exampleProperty = {
+    a:1,
+    b:2,
+    c:3,
+    d:4
+}
+
+console.log(inputProperty(exampleProperty))
+
+
+  
