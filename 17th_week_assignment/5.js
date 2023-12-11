58eff2a0965ddd0779d707400e5e3b75a0673bea
KULDEEP GANGWAR <120949786+Kuldeep8057@users.noreply.github.com>
2023-12-11 07:02:33 +0530

    Delete 17th_week_assignment directory

--- 17th_week_assignment/5.js
+++
@@ -1,9 +0,0 @@
-function greetPerson(name){
-    return new Promise((reslove)=> {
-        const greeitng = `Hello ${name}`
-        reslove(greeitng)
-    })
-}
-
-greetPerson("Mithun")
-.then((message => console.log(message)))
